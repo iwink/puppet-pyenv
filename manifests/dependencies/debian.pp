fa2a2dc432f091871f0a78a1a5af576a25681cc2
Wouter Mellema <w.mellema@iwink.nl>
2024-02-08 15:56:46 +0100

    Fixed old dependencies

--- manifests/dependencies/debian.pp
+++ manifests/dependencies/debian.pp
@@ -2,7 +2,7 @@
 #
 class pyenv::dependencies::debian {
   if ! defined(Package['build-essential'])      { package { 'build-essential':      ensure => installed } }
-  if ! defined(Package['libreadline6-dev'])     { package { 'libreadline6-dev':     ensure => installed } }
+  if ! defined(Package['libreadline-dev'])     { package { 'libreadline-dev':     ensure => installed } }
   if ! defined(Package['zlib1g'])               { package { 'zlib1g':               ensure => installed } }
   if ! defined(Package['zlib1g-dev'])           { package { 'zlib1g-dev':           ensure => installed } }
   if ! defined(Package['libssl-dev'])           { package { 'libssl-dev':           ensure => installed } }
@@ -12,7 +12,7 @@ class pyenv::dependencies::debian {
   if ! defined(Package['libxslt1-dev'])         { package { 'libxslt1-dev':         ensure => installed, alias =>'libxslt-dev' } }
   if ! defined(Package['git'])                  { package { 'git':                  ensure => installed, name => 'git-core' } }
   if ! defined(Package['curl'])                 { package { 'curl':                 ensure => installed } }
-  if ! defined(Package['libmysqlclient-dev'])   { package { 'libmysqlclient-dev':   ensure => installed } }
+  if ! defined(Package['libmariadb-dev-compat'])   { package { 'libmariadb-dev-compat':   ensure => installed } }
   if ! defined(Package['libcurl4-openssl-dev']) { package { 'libcurl4-openssl-dev': ensure => installed } }
   if ! defined(Package['libsqlite3-dev'])       { package { 'libsqlite3-dev':       ensure => installed } }
 }
