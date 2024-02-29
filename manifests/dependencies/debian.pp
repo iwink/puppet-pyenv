dac6c6db43e513bdde082e6f6686447dbed5d2b5
Harold Holsappel <h.holsappel@iwink.nl>
2024-02-29 13:54:10 +0100

    fix layout

--- manifests/dependencies/debian.pp
+++ manifests/dependencies/debian.pp
@@ -1,5 +1,20 @@
 # Install debian dependencies
 #
 class pyenv::dependencies::debian {
-  ensure_packages(['build-essential', 'libreadline-dev', 'zlib1g', 'zlib1g-dev', 'libssl-dev', 'libyaml-dev', 'libxml2-dev', 'libbz2-dev', 'libxslt1-dev', 'git', 'curl', 'libmariadb-dev-compat', 'libcurl4-openssl-dev', 'libsqlite3-dev'])
+  ensure_packages([
+    'build-essential',
+    'libreadline-dev',
+    'zlib1g',
+    'zlib1g-dev',
+    'libssl-dev',
+    'libyaml-dev',
+    'libxml2-dev',
+    'libbz2-dev',
+    'libxslt1-dev',
+    'git',
+    'curl',
+    'libmariadb-dev-compat',
+    'libcurl4-openssl-dev',
+    'libsqlite3-dev',
+  ])
 }
--- manifests/dependencies/redhat.pp
+++ manifests/dependencies/redhat.pp
@@ -1,5 +1,19 @@
 # Install redhat dependencies
 #
 class pyenv::dependencies::redhat {
-  ensure_packages(['readline-devel', 'zlib-devel', 'bzip2', 'bzip2-devel', 'openssl-devel', 'libyaml-devel', 'libxml2-devel', 'libxslt-devel', 'git', 'curl', 'mysql-libs', 'sqlite', 'sqlite-devel'])
+  ensure_packages([
+    'readline-devel',
+    'zlib-devel',
+    'bzip2',
+    'bzip2-devel',
+    'openssl-devel',
+    'libyaml-devel',
+    'libxml2-devel',
+    'libxslt-devel',
+    'git',
+    'curl',
+    'mysql-libs',
+    'sqlite',
+    'sqlite-devel',
+  ])
 }
