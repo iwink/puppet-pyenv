55ba85219ebf90e04ad5b935e1ffce395b4991d1
Harold Holsappel <h.holsappel@iwink.nl>
2024-02-29 13:51:09 +0100

    Change mechanism for detecting and installing dependencies

--- manifests/dependencies/debian.pp
+++ manifests/dependencies/debian.pp
@@ -1,18 +1,5 @@
 # Install debian dependencies
 #
 class pyenv::dependencies::debian {
-  if ! defined(Package['build-essential'])      { package { 'build-essential':      ensure => installed } }
-  if ! defined(Package['libreadline-dev'])     { package { 'libreadline-dev':     ensure => installed } }
-  if ! defined(Package['zlib1g'])               { package { 'zlib1g':               ensure => installed } }
-  if ! defined(Package['zlib1g-dev'])           { package { 'zlib1g-dev':           ensure => installed } }
-  if ! defined(Package['libssl-dev'])           { package { 'libssl-dev':           ensure => installed } }
-  if ! defined(Package['libyaml-dev'])          { package { 'libyaml-dev':          ensure => installed } }
-  if ! defined(Package['libxml2-dev'])          { package { 'libxml2-dev':          ensure => installed } }
-  if ! defined(Package['libbz2-dev'])           { package { 'libbz2-dev':           ensure => installed } }
-  if ! defined(Package['libxslt1-dev'])         { package { 'libxslt1-dev':         ensure => installed, alias =>'libxslt-dev' } }
-  if ! defined(Package['git'])                  { package { 'git':                  ensure => installed, name => 'git-core' } }
-  if ! defined(Package['curl'])                 { package { 'curl':                 ensure => installed } }
-  if ! defined(Package['libmariadb-dev-compat'])   { package { 'libmariadb-dev-compat':   ensure => installed } }
-  if ! defined(Package['libcurl4-openssl-dev']) { package { 'libcurl4-openssl-dev': ensure => installed } }
-  if ! defined(Package['libsqlite3-dev'])       { package { 'libsqlite3-dev':       ensure => installed } }
+  ensure_packages(['build-essential', 'libreadline-dev', 'zlib1g', 'zlib1g-dev', 'libssl-dev', 'libyaml-dev', 'libxml2-dev', 'libbz2-dev', 'libxslt1-dev', 'git', 'curl', 'libmariadb-dev-compat', 'libcurl4-openssl-dev', 'libsqlite3-dev'])
 }
