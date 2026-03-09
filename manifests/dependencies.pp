2ae329edd64a5d524842c76ace84b97fc5528ec2
Harold Holsappel <h.holsappel@iwink.nl>
2026-03-09 14:30:36 +0100

    fix: replace legacy $::osfamily fact with $facts hash for Puppet 8

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -1,9 +1,8 @@
-# Setup dependencies depends on os family
-#
+# @summary Install OS-specific build dependencies for pyenv.
 class pyenv::dependencies {
-  case $::osfamily {
-    debian         : { require pyenv::dependencies::debian    }
-    redhat         : { require pyenv::dependencies::redhat    }
-    default        : { notice("Could not load dependencies for ${::osfamily}") }
+  case $facts['os']['family'] {
+    'Debian'       : { require pyenv::dependencies::debian    }
+    'RedHat'       : { require pyenv::dependencies::redhat    }
+    default        : { notice("Could not load dependencies for ${facts['os']['family']}") }
   }
 }
